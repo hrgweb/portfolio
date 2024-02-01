a185e81cc8947184dda24b22d821051f15cd0646
Hergen Roque Gamayon <gamayonhergenroque.web@gmail.com>
2024-02-02 00:00:37 +0800

    enhance experience

--- components/resume/experience.vue
+++ components/resume/experience.vue
@@ -2,24 +2,25 @@
   <div class="experience">
     <h3>Work Experience</h3>
 
-    <div v-for="item in info.experience" class="item">
-      <div class="avatar">
-        <template v-if="item.avatar"
-          ><img :src="`/experience/${item.avatar}`" :alt="item.company"
-        /></template>
-        <template v-else
-          ><i class="pi pi-building" style="font-size: 2rem"></i
-        ></template>
-      </div>
-
-      <div class="item-info">
-        <h5>{{ item.position }}</h5>
-        <span>{{ item.yrsStr }}</span>
-        <span
-          ><a style="color: rgb(39, 99, 209)">{{ item.company }}</a> @{{
-            item.address
-          }}</span
-        >
+    <div class="items">
+      <div v-for="item in info.experience" class="item">
+        <div class="avatar">
+          <template v-if="item.avatar"
+            ><img :src="`/experience/${item.avatar}`" :alt="item.company"
+          /></template>
+          <template v-else
+            ><i class="pi pi-building" style="font-size: 2rem"></i
+          ></template>
+        </div>
+        <div class="item-info">
+          <h5>{{ item.position }}</h5>
+          <span>{{ item.yrsStr }}</span>
+          <span
+            ><a style="color: rgb(39, 99, 209)">{{ item.company }}</a> @{{
+              item.address
+            }}</span
+          >
+        </div>
       </div>
     </div>
   </div>
@@ -30,13 +31,30 @@ import * as info from '@/data/info.json'
 </script>
 
 <style lang="scss" scoped>
+.experience {
+  display: flex;
+  flex-direction: column;
+}
+
+.items {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+}
+
 .item {
   display: flex;
+  flex-basis: 45%;
   align-items: center;
-  margin-bottom: 1rem;
+  margin-bottom: 1.5rem;
+  margin-right: 1.5rem;
   padding: 1rem;
   border-radius: 6px;
   box-shadow: 0 0 3px rgba(60, 72, 88, 0.15) !important;
+
+  &:last-child {
+    margin-right: 0;
+  }
 
   img {
     width: 40px;
