cb72f7ae94385d84bb81517c6fbbf1de5f502e8a
Hergen Roque Gamayon <gamayonhergenroque.web@gmail.com>
2026-03-10 02:22:07 +0800

    feat: add umami analytics

--- app.vue
+++ app.vue
@@ -5,6 +5,19 @@ useHead({
   title: `Portfolio - ${info.author}`,
   htmlAttrs: { lang: 'en' }
 })
+
+onMounted(() => {
+  if (import.meta.env.DEV) return
+
+  // Create umami script
+  const s = document.createElement('script')
+  s.src = import.meta.env.VITE_UMAMI_SCRIPT_URL
+  s.setAttribute('data-website-id', import.meta.env.VITE_UMAMI_WEBSITE_ID)
+  s.async = true
+  s.defer = true
+  document.head.appendChild(s)
+  console.log('umami init: ', s)
+})
 </script>
 
 <template>
